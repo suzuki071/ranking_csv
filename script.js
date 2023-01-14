ee7024000b2f33718ad9233ef19ab916ab1c3844
suzuki071 <shinee.0711.st@gmail.com>
2023-01-14 19:50:25 +0900

    textform読み込み機能追加

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function OnButtonClick() {
-  alert('クリックされました');
+  let text_val = document.getElementById('text_value').value;
+  document.getElementById('val').textContent = text_val;
+}
+
+function ResetClick() {
+  document.getElementById('text_value').value = "";
 }
